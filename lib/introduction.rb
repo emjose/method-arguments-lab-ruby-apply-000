209fdc39bbcbdc220b69c9e4b839521c73add8e9
Emmanuel Jose <emjose@gmail.com>
2020-03-06 21:34:17 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,3 +3,10 @@ name = "Josh"
 def introduction(name)
   puts "Hi, my name is #{name}.\n"
 end
+
+name = "Dan"
+language = "Ember.js"
+
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
